024d0c562a8ab73aabe7fab7f7485a2742b43adf
Lee Atchison <lee@leeatchison.com>
2013-03-13 11:19:20 -0700

    More cleanup.

--- test/manual_test.rb
+++ test/manual_test.rb
@@ -5,15 +5,13 @@ new_relic_message = NewRelic::Plugin::NewRelicMessage.new('fake connection', 'ma
 new_relic_message.add_stat_fullname('test metric', 2, 2, :min => 1, :max => 3, :sum_of_squares => 10) 
 new_relic_message.add_stat_fullname('other metric', 2, 2, :min => 2, :max => 2, :sum_of_squares => 8) 
 
-# license_key =
-
 puts <<-EOF
 ########################################################
 Manual testing -- output curl command with payload
 ########################################################
 
- curl -vi http://staging-collector.newrelic.com/platform/v1/metrics \
-    -H "X-License-Key: #{license_key}" \
+ curl -vi http://collector.newrelic.com/platform/v1/metrics \
+    -H "X-License-Key: ADD_LICENSE_KEY" \
     -H "Content-Type: application/json" \
     -H "Accept: application/json" \
     -X POST -d \'#{new_relic_message.send(:build_request_payload)}\'
